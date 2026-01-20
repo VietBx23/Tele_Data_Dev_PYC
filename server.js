4d4698e6a425dc19dfdfb0aa12e78912c426076f
Bùi Xuân Việt <127529083+VietBx23@users.noreply.github.com>
2026-01-20 11:16:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
 const ImageKit = require("imagekit");
-
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
 
